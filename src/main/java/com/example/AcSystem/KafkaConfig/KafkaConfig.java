c1ea560a84e5b36ce68054a84f300bb06ffa7f9e
Diart435 <vlad2005646@gmail.com>
2026-04-14 23:07:13 +0400

    modded kafka

--- src/main/java/com/example/AcSystem/KafkaConfig/KafkaConfig.java
+++ src/main/java/com/example/AcSystem/KafkaConfig/KafkaConfig.java
@@ -1,2 +1,42 @@
-package com.example.AcSystem.KafkaConfig;public class KafkaConfig {
+package com.example.AcSystem.KafkaConfig;
+
+import com.example.AcSystem.JSON.KafkaJsonSerializer;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import com.example.AcSystem.Entity.Company;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JacksonJsonSerializer;
+import tools.jackson.databind.ObjectMapper;
+import tools.jackson.databind.json.JsonMapper;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class KafkaConfig {
+
+    @Bean
+    public ProducerFactory<String, Company> producerFactory(ObjectMapper objectMapper){
+        Map<String, Object> confProperties = new HashMap<>();
+        confProperties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        confProperties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+
+        DefaultKafkaProducerFactory<String, Company> factory = new DefaultKafkaProducerFactory<>(confProperties);
+
+        factory.setValueSerializer(new KafkaJsonSerializer<>(objectMapper));
+        return factory;
+    }
+
+    @Bean
+    public KafkaTemplate<String, Company> kafkaTemplate(ProducerFactory<String, Company> producerFactory){
+        return new KafkaTemplate<>(producerFactory);
+    }
 }
